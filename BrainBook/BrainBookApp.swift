2fad66ed543542e1d6f360fb68bb0703f5572917
Dearimranullah <107716481+Dearimranullah@users.noreply.github.com>
2023-01-26 15:19:09 +0500

    Initial Commit

---
+++ BrainBook/BrainBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBookApp.swift
+//  BrainBook
+//
+//  Created by Apple1 on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BrainBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
